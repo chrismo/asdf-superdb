973ec835c087ad13e57edf10924108e63cf27b52
chrismo <chrismo@clabs.org>
2025-07-24 23:45:45 -0500

    listing and installing now works with faux version

--- lib/utils.bash
+++ lib/utils.bash
@@ -31,9 +31,13 @@ list_github_tags() {
 }
 
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if superdb has other means of determining installable versions.
-	list_github_tags
+	# Read versions from versions.txt file (first column only)
+	awk '{print $1}' "${plugin_dir}/scripts/versions.txt"
+}
+
+lookup_version_sha() {
+	local version="$1"
+	awk -v ver="$version" '$1 == ver {print $2}' "${plugin_dir}/scripts/versions.txt"
 }
 
 download_release() {
@@ -45,14 +49,20 @@ install_version() {
 	local version="$2"
 	local install_path="${3%/bin}/bin"
 
+	local install_ref
 	if [ "$install_type" == "version" ]; then
-		fail "ref installs only. e.g., asdf install superdb ref:(branch|tag|commit)"
+		install_ref=$(lookup_version_sha "$version")
+		if [ -z "$install_ref" ]; then
+			fail "Version $version not found"
+		fi
+	elif [ "$install_type" == "ref" ]; then
+		install_ref="$version"
 	fi
 
 	(
 		echo "* Building $TOOL_NAME $version from github.com/brimdata/super ..."
 
-		go install github.com/brimdata/super/cmd/super@"$version"
+		go install github.com/brimdata/super/cmd/super@"$install_ref"
 
 		mkdir -p "$install_path"
 
